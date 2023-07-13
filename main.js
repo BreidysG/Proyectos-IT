00367c7867667feff7c1a904f3ae988c0a4e30fa
BreidysG <breidysgutierrez8@gmail.com>
2023-07-13 15:42:42 -0400

    Changes scroll behavior, click, logos

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const menu = document.getElementById("menu");
 const navLinks = document.getElementById("navLinks");
 const nav = document.querySelector("nav");
+const icon = document.querySelector("#menu i");
 
 
 menu.addEventListener("click", ()=>{
     navLinks.classList.toggle("visivility");
+    if(icon.classList.contains("bx")){
+            icon.classList.toggle("bx-menu");
+            icon.classList.toggle("bx-x");
+       }
 });
 
