9148327e0b4af147fe492f4feb47376b9e6b779c
BreidysG <breidysgutierrez8@gmail.com>
2023-07-21 11:08:06 -0400

    Changes correo

--- correos.php
+++ correos.php
@@ -1,5 +1,5 @@
 <?php
-    $destinatario = `info@proyectos-it.com`;
+    $destinatario = `breidysgutierrez8@gmail.com`;
     $nombre = $_POST[`nombre`];
     $correo = $_POST[`correo`];
     $asunto = $_POST[`asunto`];
@@ -12,5 +12,5 @@
     mail($destinatario, $asunto, $mensaje, $header);
     
     echo "<script>alert(`¡Correo enviado exisotamente!`)</script>";
-    echo "<script> setTimeout(\"location.href=`index.html`\", 1000)</script>";
+    echo "<script> setTimeout(\"location.href=`index.html`\", 1000) </script>";
 ?>
